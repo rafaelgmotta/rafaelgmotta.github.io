27db48a0a67760592d27a508113d5c634c4ded04
rafaelgmotta <rafaelgmotta@gmail.com>
2020-03-11 18:34:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function init(){
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 50, 50);
+  ctx.fillRect(10, 10, 20, 20);
 }
